5c1c82f70b233c7cf18a042af4a854b3e3ede354
Filipe Monteiro <filipe.monteiro.br@avenuecode.com>
2023-08-09 17:39:01 -0300

    Trying define ENVIRONMENT on Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,16 @@ import vue from '@vitejs/plugin-vue'
 
 // eslint-disable-next-line no-undef
 const Dir = __dirname;
+const ENV = process.env.VITE_VERCEL_ENV && [`development`, `preview`].includes(process.env.VITE_VERCEL_ENV)
+  ? `development`
+  : process.env.NODE_ENV;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
     'process.env': {
       ...process.env,
-      __APP_ENV__: process.env.VITE_VERCEL_ENV || process.env.NODE_ENV,
+      __APP_ENV__: ENV,
     }
   },
   plugins: [vue()],
